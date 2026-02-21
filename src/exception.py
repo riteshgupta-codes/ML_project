54d3dab7e56af51bf57004c67bc216b5273f8771
riteshgupta-codes <riteshgupta.eng@gmail.com>
2026-02-21 23:21:31 +0530

    third commit

--- src/exception.py
+++ src/exception.py
@@ -1,19 +1,20 @@
-import sys 
-import logging
-def error_message_detail(error,error_detail:sys):
-    exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occureed in python script name[{0}] line number [{1}] error message [{2}]".format(
-        file_name,exc_tb.tb_lineno,str(error))
+import sys
+from src.logger import logging
+def error_message_detail(error, error_detail:sys):
+    _, _, exc_tb = error_detail.exc_info()
 
-    return error_message    
-    
-    
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+
+    error_message = f"Error occurred in script [{file_name}] at line [{line_number}] : {str(error)}"
+    return error_message
+
+
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
 
-    def __str__self(self):
+    def __str__(self):
         return self.error_message
-    
+
